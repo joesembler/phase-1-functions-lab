56b968511c93926d6b51d82cd0776628e1aa8d5b
Joe Sembler <joeysembler@gmail.com>
2021-11-23 12:16:31 -0500

    commit tree 3549903e8e2206a90e5402ffa33c59e8b629c8c9

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street){
+    let distance;
+    if (street < 42){
+        distance = 42 - street;
+    }
+    else{
+        distance = street - 42;
+    }
+
+    return distance;
+}
+
+function distanceFromHqInFeet(street){
+    let distance;
+    if (street < 42){
+        distance = 42 - street;
+    }
+    else{
+        distance = street - 42;
+    }
+
+    return distance * 264;
+}
+
+function distanceTravelledInFeet (start, destination){
+    let distanceTravelled;
+    if (destination > start){
+        distanceTravelled = (destination - start) * 264;
+    }
+    else{
+        distanceTravelled = (start - destination) * 264;
+    }
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination){
+    let farePrice;
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        farePrice = 0;
+    }
+    else if (distance > 400 && distance < 2000){
+        farePrice = (distance - 400) * .02;
+    }
+    else if (distance >= 2000 && distance < 2500){
+        farePrice = 25;
+    }
+    else{
+        farePrice = "cannot travel that far";
+    }
+    return farePrice;
+}
